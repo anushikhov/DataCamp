492e86c335765f572f64404afdeceffa5f56682d
anushikhov <anushikhov@gmail.com>
2021-08-04 01:51:30 +0400

    Python exponentiation

---
+++ 0_Data_Scientist_with_Python/0_Introduction_to_Python/00_calc.py
@@ -0,0 +1,5 @@
+# Suppose you have $100, which you can invest with a 10% return each year.
+# After one year, it's 100 x 1.1 dollars, and after two years it's 100 x 1.1 x 1.1 = 121.
+# Add code to calculate how much money you end up with after 7 years, and print the result.
+
+print(100 * 1.1 ** 7)
