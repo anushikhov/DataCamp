03dc255f286fbeff32ec0d0db7d1916309d7076d
anushikhov <anushikhov@users.noreply.github.com>
2021-08-07 01:22:05 +0400

    Operations with NumPy array

--- 0_Data_Scientist_with_Python/0_Introduction_to_Python/10_np_array_ops.py
+++ 0_Data_Scientist_with_Python/0_Introduction_to_Python/10_np_array_ops.py
@@ -16,3 +16,22 @@ bmi = np_weight_kg / np_height_m ** 2
 
 # Print out bmi
 print(bmi)
+
+# Create the light array
+light = bmi < 21
+
+# Print out light
+print(light)
+
+# Print out BMIs of all baseball players whose BMI is below 21
+print(bmi[light])
+
+# Store weight and height lists as numpy arrays
+np_weight_lb = np.array(weight_lb)
+np_height_in = np.array(height_in)
+
+# Print out the weight at index 5
+print(np_weight_lb[5])
+
+# Print out sub-array of np_height_in: index 1 up to and including index 5
+print(np_height_in[1:6])
