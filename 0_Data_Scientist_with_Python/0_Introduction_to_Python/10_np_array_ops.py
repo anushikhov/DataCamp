528f06c8a0872f182af021d3df654e1a6c290b20
anushikhov <anushikhov@users.noreply.github.com>
2021-08-06 22:53:49 +0400

    Operations with NumPy array

---
+++ 0_Data_Scientist_with_Python/0_Introduction_to_Python/10_np_array_ops.py
@@ -0,0 +1,18 @@
+# height_in and weight_lb are available as regular lists
+height_in = [74, 72, 75, 75, 69, 82, 89, 65]
+weight_lb = [185, 175, 200, 211, 204, 180, 199, 103]
+
+# Import numpy
+import numpy as np
+
+# Create array from height_in with metric units: np_height_m
+np_height_m = np.array(height_in) * 0.0254
+
+# Create array from weight_lb with metric units: np_weight_kg
+np_weight_kg = np.array(weight_lb) * 0.453592
+
+# Calculate the BMI: bmi
+bmi = np_weight_kg / np_height_m ** 2
+
+# Print out bmi
+print(bmi)
